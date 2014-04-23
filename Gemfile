ce32a194a6707d20064d5565d1e60cfa721643bf
Ubuntu <ubuntu@domU-12-31-39-05-1D-9F.compute-1.internal>
2014-04-23 05:04:47 +0000

    implmtd httparty

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ gem 'coffee-rails', '~> 4.0.0'
 gem 'therubyracer',  platforms: :ruby
 gem 'haml-rails'
 gem 'aws-sdk', '~> 1.0'
+gem 'httparty'
+
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,15 @@
 class WelcomeController < ActionController::Base
+  require 'httparty'
+
   def index
     logger.debug('---> hely')
     mysqs = AWS::SQS.new({access_key_id:ENV['AWS_ACCESS_KEY_ID'], secret_access_key:ENV['AWS_SECRET_ACCESS_KEY']})
     logger.info("---> aws 3: #{mysqs.queues.inspect}")
     logger.info("---> aws 4: #{mysqs.queues.first.inspect}")
+    @resp = HTTParty.get('http://api.openweathermap.org/data/2.5/forecast?lat=38&lon=-122')
+    logger.info("---> weather 2: #{@resp.code.inspect}") 
+    logger.info("---> weather 3: #{@resp.message.inspect}") 
+    logger.info("---> weather 4: #{@resp.headers.inspect}") 
+    logger.info("---> weather 5: #{@resp.body.inspect}") 
   end
 end
