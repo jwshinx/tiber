55099a97a64783f830802ee9f3eaad9b88b13d23
Ubuntu <ubuntu@domU-12-31-39-05-1D-9F.compute-1.internal>
2014-04-23 04:45:41 +0000

    added aws access key

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'coffee-rails', '~> 4.0.0'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 gem 'therubyracer',  platforms: :ruby
 gem 'haml-rails'
+gem 'aws-sdk', '~> 1.0'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ActionController::Base
   def index
     logger.debug('---> hely')
+    mysqs = AWS::SQS.new({access_key_id:ENV['AWS_ACCESS_KEY_ID'], secret_access_key:ENV['AWS_SECRET_ACCESS_KEY']})
+    logger.info("---> aws 3: #{mysqs.queues.inspect}")
+    logger.info("---> aws 4: #{mysqs.queues.first.inspect}")
   end
 end
