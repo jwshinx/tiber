6c64d1ae34510048335426daaea9b954b6ca0cbe
Joel Shin <joel.shin@here.com>
2014-04-25 15:02:03 -0700

    added giggle

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,6 +7,7 @@ class WelcomeController < ActionController::Base
   # lucca
   def index
     # smile
+    # giggle 
     logger.debug('---> hely')
     mysqs = AWS::SQS.new({access_key_id:ENV['AWS_ACCESS_KEY_ID'], secret_access_key:ENV['AWS_SECRET_ACCESS_KEY']})
     logger.info("---> aws 3: #{mysqs.queues.inspect}")
