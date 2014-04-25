f0abb10e73823331f24d834c4e93ad1bfb7b2f78
Joel Shin <joel.shin@here.com>
2014-04-25 14:51:18 -0700

    added siena

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ActionController::Base
   require 'httparty'
 
+  # siena
   def index
     logger.debug('---> hely')
     mysqs = AWS::SQS.new({access_key_id:ENV['AWS_ACCESS_KEY_ID'], secret_access_key:ENV['AWS_SECRET_ACCESS_KEY']})
