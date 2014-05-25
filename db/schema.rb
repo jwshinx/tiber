c46cab13b0d06b7f3f6940a44476b496a0bd0fcb
Joel Shin <jwshin@gmail.com>
2014-05-24 20:54:26 -0700

    fixed migration schema issue

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140422035756) do
+ActiveRecord::Schema.define(version: 20140512071641) do
 
   create_table "people", force: true do |t|
     t.string   "firstname"
