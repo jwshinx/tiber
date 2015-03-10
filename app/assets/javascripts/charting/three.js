8952d2284be3dd62ed703996628531eefbc3b92c
Joel Shin <jwshin@gmail.com>
2015-03-10 08:42:20 -0700

    mentions

--- app/assets/javascripts/charting/three.js
+++ app/assets/javascripts/charting/three.js
@@ -5,8 +5,15 @@ $( document ).ready(function() {
   console.log( "---> three 1" );
   var barcode = barcodeChart();
 
-  var data = ['foo', 'bar', 'caaa', 'gumby'];
-
+  // var data = ['foo', 'bar', 'caaa', 'gumby'];
+  var data = [
+    { name: 'aaa', mentions: addData([], 100, 100), byHour: 11.1 },
+    { name: 'bbb', mentions: addData([], 100, 100), byHour: 22.2 },
+    { name: 'ccc', mentions: addData([], 100, 100), byHour: 33.3 },
+    { name: 'ddd', mentions: addData([], 100, 100), byHour: 44.4 },
+    { name: 'eee', mentions: addData([], 100, 100), byHour: 55.5 },
+  ];
+  
   var table = d3.select('#three_chart').selectAll('table')
     .data([data])
     .enter()
@@ -28,9 +35,12 @@ $( document ).ready(function() {
     .append('tr');
 
   rows.append('td')
-    .text(function(d) { return 'hello'; });
+    .text(function(d) { return d.name; });
 
-  // rows.append('td')
-  //   .text(function(d) { return 'hello'; });
+  rows.append('td')
+    .datum(function(d) { return d.mentions; })
+    .call(barcode);
 
+  rows.append('td')
+    .html(function(d) { return d.byHour; });
 });
